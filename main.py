8d2d1841356a6f93ecc11e0e876a1877e1f323f8
Gaganyadav2209 <98753163+Gaganyadav2209@users.noreply.github.com>
2024-05-25 18:37:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,6 @@ load_dotenv()
 client = Groq(
     api_key=st.secrets["Groq_API_KEY"],
 )
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 
 # def perpex_score(text):
